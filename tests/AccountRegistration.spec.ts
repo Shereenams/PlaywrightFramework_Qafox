5de6db230a8b067737dd823be45a0f6276963598
shereenams <shereenasiddique@gmail.com>
2026-06-27 15:35:32 +0530

    allure

--- tests/AccountRegistration.spec.ts
+++ tests/AccountRegistration.spec.ts
@@ -4,17 +4,15 @@ import { RegistrationPage } from '../pages/RegistrationPage';
 import { TestConfig } from '../test.config';  
 import {RandomDataUtil} from '../utils/randomDataGenerator';
 import { RegisteredData } from '../utils/registeredData';
-
-
-
-
-
+import { ReportUtil } from '../reports/reportsUtil';
 
 
 let homePage:HomePage;
 let registrationPage :RegistrationPage;
 let testConfig:TestConfig;
 let registeredData:RegisteredData;
+let gmail:string;
+let password:string;
 
         
 
@@ -33,24 +31,33 @@ test.afterEach(async ({page})=>{
 })
 
 test.describe('Account Registration Tests @s', () => {
-    test('Verify Account Registration @smoke', async () => {
-        
-        await expect(homePage.IsHomePageDisplayed()).toBeTruthy();
+    test('Verify Account Registration @smoke', async ({page},testInfo) => {
+        await ReportUtil.logStepWithScreenshot(page,testInfo,"Home Page ", async ()=>{
+            await expect(homePage.IsHomePageDisplayed()).toBeTruthy();
+          
+        })
+        await ReportUtil.logStepWithScreenshot(page,testInfo,"Registration Page ", async ()=>{
+        //await expect(homePage.IsHomePageDisplayed()).toBeTruthy();
         await homePage.clickOnMyAccount();
         await homePage.clickOnRegister();
         await registrationPage.enterFirstName(RandomDataUtil.getFirstName());     
         await registrationPage.enterLastName(RandomDataUtil.getLastName());
-        const gmail=RandomDataUtil.getEmail();
+        gmail=RandomDataUtil.getEmail();
         await console.log(gmail);
         await registrationPage.enterEmail(gmail);
-        const password=RandomDataUtil.getPassord();
+        
+        password=RandomDataUtil.getPassord();
         await console.log(password);
         await registrationPage.enterPassword(password);
         await registrationPage.enterConfirmPassword(password);
         await registrationPage.enterTelephone(RandomDataUtil.getPhoneNumber());
         await registrationPage.acceptPrivacyPolicy();
+        })
+        await ReportUtil.logStepWithScreenshot(page,testInfo,"Registration completed ", async ()=>{
+        
         await registrationPage.clickContinue();
         await expect(registrationPage.isRegistrationSuccessful()).toBeTruthy();
+        })
         await registeredData.PushRecords(gmail,password);
     
         await console.log('Faker data written to CSV successfully!');
